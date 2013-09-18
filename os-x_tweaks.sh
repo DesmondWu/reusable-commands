e370dc8be605ec2643c518c49e0655db4cff3a18
Dmitri Shuralyov <shurcooL@gmail.com>
2013-09-18 14:21:15 -0700

    Update os-x_tweaks.sh

--- os-x_tweaks.sh
+++ os-x_tweaks.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Various Xcode folders
+ls /Applications/Xcode.app/Contents/Developer/Documentation/DocSets/
+ls /Users/Dmitri/Library/Caches/com.apple.dt.Xcode/Downloads/
