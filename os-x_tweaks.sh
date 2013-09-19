d544feadbb103ae4fd9cc7b4c4b3cc90bbe8f88d
Dmitri Shuralyov <shurcooL@gmail.com>
2013-09-19 16:46:43 -0700

    Update os-x_tweaks.sh

--- os-x_tweaks.sh
+++ os-x_tweaks.sh
@@ -5,3 +5,6 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # Various Xcode folders
 /Applications/Xcode.app/Contents/Developer/Documentation/DocSets/
 ~/Library/Caches/com.apple.dt.Xcode/
+
+# Port Limits
+sudo sysctl -w net.inet.tcp.msl=1000
