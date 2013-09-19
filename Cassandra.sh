5ee61f3d448aacc4c9326baf8842578f7f58c2e6
Dmitri Shuralyov <shurcooL@gmail.com>
2013-09-19 16:49:53 -0700

    Update Cassandra.sh

--- Cassandra.sh
+++ Cassandra.sh
@@ -1,4 +1,4 @@
-http://www.datastax.com/documentation/gettingstarted/index.html?pagename=docs&version=quick_start&file=quickstart#getting_started/gettingStartedTar_t.html
+# http://www.datastax.com/documentation/gettingstarted/index.html?pagename=docs&version=quick_start&file=quickstart#getting_started/gettingStartedTar_t.html
 
 wget "http://apache.mirrors.pair.com/cassandra/2.0.0/apache-cassandra-2.0.0-bin.tar.gz"
 tar -xvzf apache-cassandra-2.0.0-bin.tar.gz
@@ -11,6 +11,8 @@ sudo mkdir -p /var/log/cassandra/
 
 sed -i 's|rpc_address: localhost|rpc_address: 0.0.0.0|g' conf/cassandra.yaml
 
+# Start Cassandra
 sudo bin/cassandra
 
+# Stop
 sudo pkill -f CassandraDaemon
