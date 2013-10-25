d9d1911311f3c93826297830f215cfcf21159173
Dmitri Shuralyov <shurcooL@gmail.com>
2013-10-24 23:50:41 -0700

    Update go-gl_glfw3_install.sh

--- go-gl_glfw3_install.sh
+++ go-gl_glfw3_install.sh
@@ -25,3 +25,4 @@ cp ./include/GLFW/glfw3.h /usr/local/include/GLFW/
 cp ./src/libglfw3.a /usr/local/lib/
 
 go get "github.com/go-gl/glfw3"
+# CGO_CFLAGS="-I/usr/local/include/" CGO_LDFLAGS="-L/usr/local/lib/" go install "github.com/go-gl/glfw3"
