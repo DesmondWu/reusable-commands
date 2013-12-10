e2ec7d411d461e85af935f1bc5a285a7b61f564f
Dmitri Shuralyov <shurcooL@gmail.com>
2013-12-09 21:22:48 -0800

    Update go-gl_glfw3_install.sh

--- go-gl_glfw3_install.sh
+++ go-gl_glfw3_install.sh
@@ -1,28 +1,16 @@
-# Do patch
+# Optional patch
 #//SetFrontProcess(&psn);
 #[window->ns.object setLevel:NSFloatingWindowLevel];
-
-
-cmake . -DBUILD_SHARED_LIBS=ON
-make
-
-
-mkdir -p /usr/local/include/GLFW/
-cp ./include/GLFW/glfw3.h /usr/local/include/GLFW/
-cp -a ./src/libglfw*.dylib /usr/local/lib/
-mv /usr/local/lib/libglfw.dylib /usr/local/lib/libglfw3.dylib
-
-
-CC=clang go get -u "github.com/go-gl/glfw3"
 
 
 # Go 1.2
 cmake .
 make
 
+# Alternatively, just `sudo make install`
 mkdir -p /usr/local/include/GLFW/
 cp ./include/GLFW/glfw3.h /usr/local/include/GLFW/
 cp ./src/libglfw3.a /usr/local/lib/
 
-go get "github.com/go-gl/glfw3"
+go get -u "github.com/go-gl/glfw3"
 # CGO_CFLAGS="-I/usr/local/include/" CGO_LDFLAGS="-L/usr/local/lib/" go install "github.com/go-gl/glfw3"
