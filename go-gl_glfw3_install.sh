f1fade376caf2c1272b182f3d1eaccc82635e44b
Dmitri Shuralyov <shurcooL@gmail.com>
2014-03-27 09:33:02 +0000

    Update go-gl_glfw3_install.sh

--- go-gl_glfw3_install.sh
+++ go-gl_glfw3_install.sh
@@ -19,3 +19,18 @@ cp ./src/libglfw3.a /usr/local/lib/
 
 go get -u "github.com/go-gl/glfw3"
 # CGO_CFLAGS="-I/usr/local/include/" CGO_LDFLAGS="-L/usr/local/lib/" go install "github.com/go-gl/glfw3"
+
+# Linux
+# =====
+
+# Make sure glfw (or any previous version) is not already installed
+sudo apt-get update
+sudo apt-get install build-essential cmake git xorg-dev libglu1-mesa-dev
+git clone https://github.com/glfw/glfw.git /tmp/glfw
+cd /tmp/glfw
+git checkout latest
+cmake . -DBUILD_SHARED_LIBS=ON
+make
+sudo make install
+sudo ldconfig
+# rm -rf /tmp/glfw
