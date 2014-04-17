ed33ae496f36aaea735a1d183f77e833c92a9f3d
Dmitri Shuralyov <shurcooL@gmail.com>
2014-04-16 19:08:00 -0700

    Update go-gl_glfw3_install.sh

--- go-gl_glfw3_install.sh
+++ go-gl_glfw3_install.sh
@@ -1,14 +1,11 @@
+# OS X
+# ====
+
 # GLFW
 git clone https://github.com/glfw/glfw.git
 cd ./glfw
 git checkout latest
 
-# Optional patch
-#//SetFrontProcess(&psn);
-#[window->ns.object setLevel:NSFloatingWindowLevel];
-
-
-# Go 1.2
 cmake .
 make
 
@@ -18,7 +15,6 @@ cp ./include/GLFW/glfw3.h /usr/local/include/GLFW/
 cp ./src/libglfw3.a /usr/local/lib/
 
 go get -u "github.com/go-gl/glfw3"
-# CGO_CFLAGS="-I/usr/local/include/" CGO_LDFLAGS="-L/usr/local/lib/" go install "github.com/go-gl/glfw3"
 
 # Linux
 # =====
